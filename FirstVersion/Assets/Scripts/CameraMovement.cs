a075542a8e704dd1b902324537f29ce479e2c779
Aaron Sengier <aaron.sengier@gmail.com>
2023-12-13 16:20:08 +0100

    try

--- FirstVersion/Assets/Scripts/CameraMovement.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5a50099f01948383676fb13e412baa3a5375f54dcac82d8650a366fef3e907f2
-size 326
--- FirstVersion/Assets/Scripts/PlayerMovement.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ee25e13e37d097b7dd85dd578c0363b5bb7509f2c8a4fec74fdf5e90efde8f8d
-size 976
